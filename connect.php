85978c5ef14bf66271780f849bd2fdf686171e8c
KASSOUMH <hadykassoum2003@gmail.com>
2025-03-27 11:20:26 +0100

    blah blah

--- connect.php
+++ connect.php
@@ -4,5 +4,5 @@ $dbname = getenv('DB_NAME') ?: 'mabase';
 $user = getenv('DB_USER') ?: 'myuser';
 $pass = getenv('DB_PASS') ?: 'mypassword';
 
-$conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+$conn = new PDO("mysql:host=c_mariadb;dbname=mabase", "myuser", "mypassword");
 ?>
